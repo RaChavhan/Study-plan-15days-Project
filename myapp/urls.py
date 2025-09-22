2e3b632b9921fd5f6ddd8e42ff0833f387136269
ranjitch03 <Rnchavhan@outlook.com>
2025-09-22 07:56:27 +0530

    bit change in URL.py path

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,11 +1,17 @@
-from django.urls import path, include
+from django.urls import path
 from .views import RegisterAPI, ProfileAPI
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     # path("index/",views.index,name='index'),
-    
     path('register/', RegisterAPI.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('profile/', ProfileAPI.as_view(), name='profile'),
+    
 
 ]
